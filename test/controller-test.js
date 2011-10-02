b9fcb4d85d84320e666e4d89d9dfdae5c71d047b
Jared Hanson <jaredhanson@gmail.com>
2011-10-02 16:11:22 -0700

    Add param() function to Controller.

--- test/controller-test.js
+++ test/controller-test.js
@@ -7,6 +7,11 @@ var Controller = require('locomotive/controller');
 /* MockRequest */
 
 function MockRequest() {
+  this._params = {};
+}
+
+MockRequest.prototype.param = function(name, defaultValue) {
+  return this._params[name] || defaultValue;
 }
 
 
@@ -51,9 +56,15 @@ vows.describe('Controller').addBatch({
       // TODO: Refactor controller naming.
       TestController._name = 'test';
       
-      TestController.show = function() {
+      TestController.setProperties = function() {
         this.title = 'The Foo Is Barred';
         this.author = 'Neckbeard';
+        this.render();
+      }
+      
+      TestController.showPropertiesBasedOnParam = function() {
+        this.id = this.param('id');
+        this.fullText = this.param('full_text', 'true');
         this.render();
       }
       
@@ -61,7 +72,7 @@ vows.describe('Controller').addBatch({
       return instance;
     },
     
-    'invoking show': {
+    'invoking a method which sets properties': {
       topic: function(controller) {
         var self = this;
         var req, res;
@@ -71,7 +82,7 @@ vows.describe('Controller').addBatch({
           self.callback(null, controller, req, res);
         });
         controller._prepare(req, res);
-        controller._invoke('show');
+        controller._invoke('setProperties');
       },
       
       'should assign controller properties as response locals': function(err, c, req, res) {
@@ -80,6 +91,29 @@ vows.describe('Controller').addBatch({
         assert.equal(res._locals[0].val, 'The Foo Is Barred');
         assert.equal(res._locals[1].name, 'author');
         assert.equal(res._locals[1].val, 'Neckbeard');
+      },
+    },
+    
+    'invoking a method which sets properties based on param': {
+      topic: function(controller) {
+        var self = this;
+        var req, res;
+        
+        req = new MockRequest();
+        req._params['id'] = '123456';
+        res = new MockResponse(function() {
+          self.callback(null, controller, req, res);
+        });
+        controller._prepare(req, res);
+        controller._invoke('showPropertiesBasedOnParam');
+      },
+      
+      'should assign controller properties as response locals': function(err, c, req, res) {
+        assert.length(res._locals, 2);
+        assert.equal(res._locals[0].name, 'id');
+        assert.equal(res._locals[0].val, '123456');
+        assert.equal(res._locals[1].name, 'fullText');
+        assert.equal(res._locals[1].val, 'true');
       },
     },
   },
