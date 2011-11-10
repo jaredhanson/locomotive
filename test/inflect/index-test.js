173ea6af2366efe3c030c3a31ca95a0fb9111d3c
Jared Hanson <jaredhanson@gmail.com>
2011-11-09 20:01:27 -0800

    Refactor controllerize and decontrollerize inflections.

--- test/inflect/index-test.js
+++ test/inflect/index-test.js
@@ -9,8 +9,9 @@ vows.describe('Module').addBatch({
   'inflect': {
     'should export lingo functions': function () {
       assert.isFunction(inflect.capitalize);
-      assert.isFunction(inflect.camelcase);
       assert.isFunction(inflect.join);
+      assert.isFunction(inflect.en.pluralize);
+      assert.isFunction(inflect.en.singularize);
     },
   },
   
@@ -23,30 +24,36 @@ vows.describe('Module').addBatch({
     },
   },
   
-  'controllerize': {
-    'should append Controller to string': function () {
-      assert.equal(inflect.controllerize('Foo'), 'FooController');
-    },
-    'should not append Controller to string if it exists': function () {
-      assert.equal(inflect.controllerize('FooController'), 'FooController');
-    },
-  },
-  
-  'decontrollerize': {
-    'should remove Controller from string': function () {
-      assert.equal(inflect.decontrollerize('FooController'), 'Foo');
-    },
-    'should not remove Controller from string if it does not exists': function () {
-      assert.equal(inflect.decontrollerize('Foo'), 'Foo');
-    },
-  },
-  
   'underscore': {
     'should underscore camelcase words': function () {
       assert.equal(inflect.underscore('FooBar'), 'foo_bar');
     },
     'should underscore camelcase words with consequtive capital letters': function () {
       assert.equal(inflect.underscore('SSLError'), 'ssl_error');
+    },
+  },
+  
+  'controllerize': {
+    'should append Controller to string': function () {
+      assert.equal(inflect._controllerize('Foo'), 'FooController');
+    },
+    'should not append Controller to string if it exists': function () {
+      assert.equal(inflect._controllerize('FooController'), 'FooController');
+    },
+    'should camelize string': function () {
+      assert.equal(inflect._controllerize('foo_bar_baz'), 'FooBarBazController');
+    },
+  },
+  
+  'decontrollerize': {
+    'should remove Controller from string': function () {
+      assert.equal(inflect._decontrollerize('FooController'), 'foo');
+    },
+    'should not remove Controller from string if it does not exists': function () {
+      assert.equal(inflect._decontrollerize('Foo'), 'foo');
+    },
+    'should underscore string': function () {
+      assert.equal(inflect._decontrollerize('FooBarBazController'), 'foo_bar_baz');
     },
   },
   
