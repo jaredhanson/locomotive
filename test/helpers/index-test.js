081c06662853215bdee198097e3e73a3d854e322
Jared Hanson <jaredhanson@gmail.com>
2011-11-09 21:01:31 -0800

    Add helpers.

---
+++ test/helpers/index-test.js
@@ -0,0 +1,15 @@
+var vows = require('vows');
+var assert = require('assert');
+var util = require('util');
+var helpers = require('locomotive/helpers');
+
+
+vows.describe('Helpers').addBatch({
+  
+  'url helpers': {
+    'should be exported': function () {
+      assert.isFunction(helpers.linkTo);
+    },
+  },
+  
+}).export(module);
---
+++ test/helpers/url-test.js
@@ -0,0 +1,15 @@
+var vows = require('vows');
+var assert = require('assert');
+var util = require('util');
+var helpers = require('locomotive/helpers/url');
+
+
+vows.describe('URLHelpers').addBatch({
+  
+  'linkTo': {
+    'should build correct tag with name and url': function () {
+      assert.equal(helpers.linkTo('My Account', '/account'), '<a href="/account">My Account</a>');
+    },
+  },
+  
+}).export(module);
