387e8d71840236c6c86a2e2213620c79ff4e19e5
Jared Hanson <jaredhanson@gmail.com>
2011-11-09 23:01:45 -0800

    Additional documentation and tests for Controller.

--- test/controller-test.js
+++ test/controller-test.js
@@ -2,6 +2,7 @@ var vows = require('vows');
 var assert = require('assert');
 var util = require('util');
 var Controller = require('locomotive/controller');
+var ControllerError = require('locomotive/errors/controllererror');
 
 
 /* MockRequest */
@@ -68,23 +69,35 @@ vows.describe('Controller').addBatch({
       var TestController = new Controller();
       TestController._init('TestController');
       
-      TestController.setProperties = function() {
-        this.title = 'The Foo Is Barred';
-        this.author = 'Neckbeard';
+      TestController.showOnTheRoad = function() {
+        this.title = 'On The Road';
+        this.author = 'Jack Kerouac';
         this.render();
       }
       
-      TestController.showPropertiesBasedOnParam = function() {
+      TestController.showBookById = function() {
         this.id = this.param('id');
         this.fullText = this.param('full_text', 'true');
         this.render();
+      }
+      
+      TestController.redirectHome = function() {
+        this.redirect('/home');
+      }
+      
+      TestController.redirectHome303 = function() {
+        this.redirect('/home', 303);
+      }
+      
+      TestController.somethingGoesWrong = function() {
+        this.error(new Error('something goes wrong'));
       }
       
       var instance = Object.create(TestController);
       return instance;
     },
     
-    'invoking a method which sets properties': {
+    'invoking an action which sets properties and renders': {
       topic: function(controller) {
         var self = this;
         var req, res;
@@ -94,19 +107,19 @@ vows.describe('Controller').addBatch({
           self.callback(null, controller, req, res);
         });
         controller._prepare(req, res);
-        controller._invoke('setProperties');
+        controller._invoke('showOnTheRoad');
       },
       
       'should assign controller properties as response locals': function(err, c, req, res) {
         assert.length(res._locals, 2);
         assert.equal(res._locals[0].name, 'title');
-        assert.equal(res._locals[0].val, 'The Foo Is Barred');
+        assert.equal(res._locals[0].val, 'On The Road');
         assert.equal(res._locals[1].name, 'author');
-        assert.equal(res._locals[1].val, 'Neckbeard');
+        assert.equal(res._locals[1].val, 'Jack Kerouac');
       },
     },
     
-    'invoking a method which sets properties based on param': {
+    'invoking an action which checks params, sets properties, and renders': {
       topic: function(controller) {
         var self = this;
         var req, res;
@@ -117,7 +130,7 @@ vows.describe('Controller').addBatch({
           self.callback(null, controller, req, res);
         });
         controller._prepare(req, res);
-        controller._invoke('showPropertiesBasedOnParam');
+        controller._invoke('showBookById');
       },
       
       'should assign controller properties as response locals': function(err, c, req, res) {
@@ -126,6 +139,102 @@ vows.describe('Controller').addBatch({
         assert.equal(res._locals[0].val, '123456');
         assert.equal(res._locals[1].name, 'fullText');
         assert.equal(res._locals[1].val, 'true');
+      },
+    },
+    
+    'invoking an action which redirects': {
+      topic: function(controller) {
+        var self = this;
+        var req, res;
+        
+        req = new MockRequest();
+        res = new MockResponse(function() {
+          self.callback(new Error('should not be called'));
+        });
+        res.redirect = function(url, status) {
+          self.callback(null, url, status);
+        }
+        
+        controller._prepare(req, res);
+        controller._invoke('redirectHome');
+      },
+      
+      'should redirect': function(err, url, status) {
+        assert.equal(url, '/home');
+        assert.isUndefined(status);
+      },
+    },
+    
+    'invoking an action which redirects with a status code': {
+      topic: function(controller) {
+        var self = this;
+        var req, res;
+        
+        req = new MockRequest();
+        res = new MockResponse(function() {
+          self.callback(new Error('should not be called'));
+        });
+        res.redirect = function(url, status) {
+          self.callback(null, url, status);
+        }
+        
+        controller._prepare(req, res);
+        controller._invoke('redirectHome303');
+      },
+      
+      'should redirect': function(err, url, status) {
+        assert.equal(url, '/home');
+        assert.equal(status, 303);
+      },
+    },
+    
+    'invoking an action which encounters an error': {
+      topic: function(controller) {
+        var self = this;
+        var req, res, next;
+        
+        req = new MockRequest();
+        res = new MockResponse(function() {
+          self.callback(new Error('should not be called'));
+        });
+        next = function(err) {
+          self.callback(null, err);
+        }
+        
+        controller._prepare(req, res, next);
+        controller._invoke('somethingGoesWrong');
+      },
+      
+      'should not send response' : function(err, e) {
+        assert.isNull(err);
+      },
+      'should call next with error': function(err, e) {
+        assert.instanceOf(e, Error);
+      },
+    },
+    
+    'invoking an action which does not exist': {
+      topic: function(controller) {
+        var self = this;
+        var req, res, next;
+        
+        req = new MockRequest();
+        res = new MockResponse(function() {
+          self.callback(new Error('should not be called'));
+        });
+        next = function(err) {
+          self.callback(null, err);
+        }
+        
+        controller._prepare(req, res, next);
+        controller._invoke('unknown');
+      },
+      
+      'should not send response' : function(err, e) {
+        assert.isNull(err);
+      },
+      'should call next with error': function(err, e) {
+        assert.instanceOf(e, ControllerError);
       },
     },
   },
