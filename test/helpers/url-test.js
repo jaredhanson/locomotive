1096cdf3f1076cfb8dfa056570fb79592b9ec9d8
Jared Hanson <jaredhanson@gmail.com>
2011-11-19 18:20:32 -0800

    Add route awareness to linkTo helper.

--- test/helpers/url-test.js
+++ test/helpers/url-test.js
@@ -1,14 +1,100 @@
 var vows = require('vows');
 var assert = require('assert');
 var util = require('util');
+var Route = require('locomotive/route');
+var dynamicHelpers = require('locomotive/helpers/dynamic');
 var helpers = require('locomotive/helpers/url');
+
+
+/* MockLocomotive */
+
+function MockLocomotive() {
+  var self = this;
+  this._routes = {};
+  this._routes['AnimalsController#show'] = new Route('get', '/animals/:id');
+  this._routes['ProfileController#show'] = new Route('get', '/profile');
+  this._routes._find = function(controller, action) {
+    var key = controller + '#' + action;
+    return self._routes[key];
+  }
+  
+  function animalURL(obj) {
+    return this.urlFor({ controller: 'AnimalsController', action: 'show', id: obj.id });
+  }
+  this.routingHelpers = { animalURL: animalURL };
+}
+
+MockLocomotive.prototype._recordOf = function(obj) {
+  if (typeof obj === 'object') { return obj.constructor.name; }
+  return null;
+}
+
+/* MockRequest */
+
+function MockRequest() {
+}
+
+/* MockResponse */
+
+function MockResponse(fn) {
+}
+
+/* util */
+
+function augment(a, b) {
+  for (var method in b) {
+    a[method] = b[method];
+  }
+}
 
 
 vows.describe('URLHelpers').addBatch({
   
   'linkTo': {
-    'should build correct tag with name and url': function () {
+    'should build correct tag with text and url': function () {
       assert.equal(helpers.linkTo('My Account', '/account'), '<a href="/account">My Account</a>');
+    },
+    'should build correct tag with text, url, and options': function () {
+      assert.equal(helpers.linkTo('My Account', '/account', { rel: 'me' }), '<a rel="me" href="/account">My Account</a>');
+    },
+    
+    'route awareness': {
+      topic: function() {
+        var app = new MockLocomotive();
+        var req = new MockRequest();
+        req.headers = { 'host': 'www.example.com' };
+        req.locomotive = app;
+        req.controller = 'TestController';
+        req.action = 'index';
+        var res = new MockResponse();
+        var view = new Object();
+        var dynHelpers = {};
+        for (var key in dynamicHelpers) {
+          dynHelpers[key] = dynamicHelpers[key].call(this, req, res);
+        }
+        var routingHelpers = {};
+        for (var key in app.routingHelpers) {
+          routingHelpers[key] = app.routingHelpers[key].bind(view);
+        }
+
+        augment(view, helpers);
+        augment(view, dynHelpers);
+        augment(view, routingHelpers);
+        return view;
+      },
+      
+      'should construct link to controller and action': function (view) {
+        assert.equal(view.linkTo('Profile', { controller: 'ProfileController', action: 'show' }), '<a href="http://www.example.com/profile">Profile</a>');
+        assert.equal(view.linkTo('Profile', { controller: 'ProfileController', action: 'show' }, { rel: 'me' }), '<a rel="me" href="http://www.example.com/profile">Profile</a>');
+      },
+      'should construct link to resource': function (view) {
+        function Animal() {};
+        var animal = new Animal();
+        animal.id = '123';
+        
+        assert.equal(view.linkTo('An Animal', animal), '<a href="http://www.example.com/animals/123">An Animal</a>');
+        assert.equal(view.linkTo('An Animal', animal, { rel: 'pet' }), '<a rel="pet" href="http://www.example.com/animals/123">An Animal</a>');
+      },
     },
   },
   
