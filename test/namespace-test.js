8e43e426b170ad61552b6fd61a9892950857a9b3
Jared Hanson <jaredhanson@gmail.com>
2012-01-22 23:21:41 -0800

    Implment path and controller qualifiers.

--- test/namespace-test.js
+++ test/namespace-test.js
@@ -6,7 +6,7 @@ var Namespace = require('locomotive/namespace');
 
 vows.describe('Namespace').addBatch({
   
-  'namespace constructed without arguments': {
+  'default namespace': {
     topic: function() {
       return new Namespace();
     },
@@ -20,6 +20,13 @@ vows.describe('Namespace').addBatch({
     'should not have parent property': function (ns) {
       assert.isNull(ns.parent);
     },
+    'should qualify controllers': function (ns) {
+      assert.equal(ns.qcontroller('PhotosController'), 'PhotosController');
+    },
+    'should qualify paths': function (ns) {
+      assert.equal(ns.qpath('photos'), '/photos');
+      assert.equal(ns.qpath('/photos'), '/photos');
+    },
   },
   
   'namespace constructed with name': {
@@ -31,10 +38,64 @@ vows.describe('Namespace').addBatch({
       assert.equal(ns.name, 'foo');
     },
     'should have module property': function (ns) {
-      assert.equal(ns.module, 'foo');
+      assert.equal(ns.module, 'Foo');
     },
     'should not have parent property': function (ns) {
       assert.isNull(ns.parent);
+    },
+    'should qualify controllers': function (ns) {
+      assert.equal(ns.qcontroller('PhotosController'), 'Foo::PhotosController');
+    },
+    'should qualify paths': function (ns) {
+      assert.equal(ns.qpath('photos'), '/foo/photos');
+      assert.equal(ns.qpath('/photos'), '/foo/photos');
+    },
+  },
+  
+  'namespace constructed with name and module option': {
+    topic: function() {
+      return new Namespace('foo', { module: 'Bar' });
+    },
+    
+    'should have name property': function (ns) {
+      assert.equal(ns.name, 'foo');
+    },
+    'should have module property': function (ns) {
+      assert.equal(ns.module, 'Bar');
+    },
+    'should not have parent property': function (ns) {
+      assert.isNull(ns.parent);
+    },
+    'should qualify controllers': function (ns) {
+      assert.equal(ns.qcontroller('PhotosController'), 'Bar::PhotosController');
+    },
+    'should qualify paths': function (ns) {
+      assert.equal(ns.qpath('photos'), '/foo/photos');
+      assert.equal(ns.qpath('/photos'), '/foo/photos');
+    },
+  },
+  
+  'namespace with parent': {
+    topic: function() {
+      var admin = new Namespace('net');
+      return new Namespace('http', {}, admin);
+    },
+    
+    'should have empty name property': function (ns) {
+      assert.equal(ns.name, 'http');
+    },
+    'should have empty module property': function (ns) {
+      assert.equal(ns.module, 'Http');
+    },
+    'should have parent property': function (ns) {
+      assert.isObject(ns.parent);
+    },
+    'should qualify controllers': function (ns) {
+      assert.equal(ns.qcontroller('ProxiesController'), 'Net::Http::ProxiesController');
+    },
+    'should qualify paths': function (ns) {
+      assert.equal(ns.qpath('proxies'), '/net/http/proxies');
+      assert.equal(ns.qpath('/proxies'), '/net/http/proxies');
     },
   },
 
