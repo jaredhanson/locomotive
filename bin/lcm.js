8fea28db25c640ff0071fb6884b4851c2fcca04b
Jared Hanson <jaredhanson@gmail.com>
2012-01-28 14:03:47 -0800

    Add option to specify app directory.

--- bin/lcm.js
+++ bin/lcm.js
@@ -4,6 +4,7 @@ var program = require('commander')
   , locomotive = require('../');
 
 program.version(locomotive.version)
+  .option('-a, --app [directory]', 'load app at specified directory (default: <working-dir>)')
   .option('-p, --port [number]', 'listen on specified port (default: 3000)');
 
 program.command('create')
@@ -15,7 +16,7 @@ program.command('create')
 program.command('server')
   .description('-> start the Locomotive server')
   .action(function() {
-    locomotive.cli.server(program.port || 3000);
+    locomotive.cli.server(program.app || process.cwd(), program.port || 3000);
   });
 
 program.parse(process.argv);
