52db0616d4b6d1cb27ae9644d3ce09763fc4e334
goddyzhao <goddy128@gmail.com>
2012-01-31 17:51:34 +0800

    add node native debug mode

--- bin/lcm.js
+++ bin/lcm.js
@@ -5,7 +5,10 @@ var program = require('commander')
 
 program.version(locomotive.version)
   .option('-a, --app [directory]', 'load app at specified directory (default: <working-dir>)')
-  .option('-p, --port [number]', 'listen on specified port (default: 3000)');
+  .option('-p, --port [number]', 'listen on specified port (default: 3000)')
+  .option('--debug', 'enable node debug mode')
+  .option('--debug-brk', 'enable node debug-brk mode')
+  .option('--debug-port [number]', 'debug app on specified port(default: 5858)');
 
 program.command('create')
   .description('-> create Locomotive application')
@@ -16,7 +19,20 @@ program.command('create')
 program.command('server')
   .description('-> start the Locomotive server')
   .action(function() {
-    locomotive.cli.server(program.app || process.cwd(), program.port || 3000);
+  	var debug = program.debug || program.debugBrk;
+  	if(debug){
+  		var debugMode = program.debug ? "--debug" : "--debug-brk"
+  		  , filePath = require("path").join(__dirname, "server.js")
+  		  , command = ["node ", debugMode, " ", filePath].join("");
+  		
+  		require("child_process")
+  			.exec(command,function(err, stdout, stderr){
+  				if(err){ throw err; }
+  				console.log(stdout);
+  			});
+  	}else{
+	    locomotive.cli.server(program.app || process.cwd(), program.port || 3000);
+  	}
   });
 
 program.parse(process.argv);
