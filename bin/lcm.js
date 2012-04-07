f4806887ae6d0001b4d4826502d587fcee88ee00
Jared Hanson <jaredhanson@gmail.com>
2012-04-07 16:05:32 -0700

    Add more help to server command.

--- bin/lcm.js
+++ bin/lcm.js
@@ -13,12 +13,13 @@ program.command('create')
   });
 
 program.command('server')
-  .description('-> start the Locomotive server')
+  .description('-> start Locomotive server')
   .option('-a, --address [address]', 'listen on specified address (default: 0.0.0.0)')
   .option('-p, --port [port]', 'listen on specified port (default: 3000)', parseInt)
   .option('-e, --env [environment]', 'run in specified environment (default: development)')
   .option('--debug [port]', 'enable V8 debugger on specified port (default: 5858)', parseInt)
   .option('--debug-brk [port]', 'enable V8 debugger on specified port and break immediately (default: 5858)', parseInt)
+  .option('--more', 'use with --help to show more help')
   .action(function(options) {
     options = options || {};
     options.address = options.address || '0.0.0.0';
@@ -29,10 +30,28 @@ program.command('server')
     
     locomotive.cli.server(program.app || process.cwd(), options.address, options.port, options.env, options);
   }).on('--help', function(options) {
-    if (program.rawArgs.indexOf('--more') != -1) {
-      console.log('  Debugging:');
+    if (program.rawArgs && program.rawArgs.indexOf('--more') != -1) {
+      console.log("  Debugging:");
       console.log();
-      console.log('    $ lcm server --debug');
+      console.log("    Locomotive applications can be debugged by enabling debug mode.");
+      console.log();
+      console.log("        $ lcm server --debug");
+      console.log("        $ lcm server --debug-brk");
+      console.log();
+      console.log("    Debug mode activates V8's debugger protocol, so standard Node tools can be");
+      console.log("    used.  For example, with Locomotive running with debug mode enabled, launch");
+      console.log("    node-inspector:");
+      console.log();
+      console.log("        $ node-inspector");
+      console.log();
+      console.log("    Then launch any WebKit-based browser, such as Chrome or Safari, and open");
+      console.log("    http://0.0.0.0:8080/debug?port=5858.  The conventional Web Inspector can be");
+      console.log("    used to debug server-side applications.");
+      console.log();
+      console.log("    Verbose log messages will be written to the console when `locomotive` is set");
+      console.log("    in the DEBUG environment variable.");
+      console.log();
+      console.log("        $ DEBUG=locomotive lcm server");
       console.log();
     }
   });
