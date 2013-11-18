18cf9a32965df356d8bfc17579808f7c33b0011f
Jared Hanson <jaredhanson@gmail.com>
2013-11-18 09:50:37 -0800

    Refactor controller creation into app.

--- test/middleware/dispatch.test.js
+++ test/middleware/dispatch.test.js
@@ -6,57 +6,63 @@ function MockApplication() {
   this._controllers = {};
 }
 
-MockApplication.prototype.controller = function(name, controller) {
-  if (controller) {
-    this._controllers[name] = controller;
-    return this;
+MockApplication.prototype._controller = function(id, cb) {
+  var ctrl = this._controllers[id];
+  if (!ctrl) {
+    return cb(new Error("Unable to create controller '" + id + "'"));
   }
-  return this._controllers[name];
+  return cb(null, ctrl);
 }
 
 
-function MockController(cb) {
-  this._prep = cb;
+function ReqResController() {
 }
 
-MockController.prototype._init = function(req, res, next) {
-  if (req.method !== 'GET') { throw new Error('Controller initialized with invalid request'); }
-  if (typeof res.setHeader !== 'function') { throw new Error('Controller initialized with invalid response'); }
-  if (typeof next !== 'function') { throw new Error('Controller initialized with invalid next function'); }
-  
-  if (this._prep) { this._prep(this); }
-  
+ReqResController.prototype._init = function(req, res, next) {
   this.req = req;
   this.res = res;
   this.next = next;
 }
 
-MockController.prototype._invoke = function(action) {
-  this.res.end('Invoked action: ' + action);
+ReqResController.prototype._invoke = function(action) {
+  this.res.end(this.req.url + ' -> ' + action);
+}
+
+
+function NextController() {
+}
+
+NextController.prototype._init = function(req, res, next) {
+  this.req = req;
+  this.res = res;
+  this.next = next;
+}
+
+NextController.prototype._invoke = function(action) {
+  this.next();
 }
 
 
 
 
-describe('underlay/express/middleware/dispatch', function() {
+describe('middleware/dispatch', function() {
   
   it('should be named dispatch', function() {
     expect(dispatch().name).to.equal('dispatch');
   });
   
-  describe('dispatching to controller prototype', function() {
-    var controller, request, response;
+  describe('dispatching to controller that uses req and res', function() {
+    var request, response;
     
-    var app = new MockApplication();
-    var proto = new MockController(function(ctl) {
-      controller = ctl;
-    });
-    app.controller('foo', proto);
+    var app = new MockApplication()
+      , controller = new ReqResController();
+    app._controllers['robots'] = controller;
 
     before(function(done) {
-      chai.connect(dispatch(app, 'foo', 'bar'))
+      chai.connect(dispatch(app, 'robots', 'beepBoop'))
         .req(function(req) {
           request = req;
+          req.url = '/robots/noise';
         })
         .end(function(res) {
           response = res;
@@ -65,38 +71,50 @@ describe('underlay/express/middleware/dispatch', function() {
         .dispatch();
     });
     
-    it('should create instance from prototype', function() {
-      expect(controller.__proto__).to.equal(proto);
-      expect(controller).to.be.an.instanceOf(MockController);
-    });
-    
     it('should respond', function() {
       expect(response.statusCode).to.equal(200);
-      expect(response.body).to.equal('Invoked action: bar');
+      expect(response.body).to.equal('/robots/noise -> beepBoop');
+    });
+  });
+  
+  describe('dispatching to controller that uses next', function() {
+    var error;
+    
+    var app = new MockApplication()
+      , controller = new NextController();
+    app._controllers['robots'] = controller;
+
+    before(function(done) {
+      chai.connect(dispatch(app, 'robots', 'beepBoop'))
+        .next(function(err) {
+          error = err;
+          done();
+        })
+        .dispatch();
+    });
+    
+    it('should call next without error', function() {
+      expect(error).to.be.undefined;
     });
   });
   
   describe('dispatching to non-existant controller', function() {
-    var err;
+    var error;
     
     var app = new MockApplication();
 
     before(function(done) {
       chai.connect(dispatch(app, 'invalid', 'index'))
-        .req(function(req) {
-          request = req;
-        })
-        .next(function(e) {
-          err = e;
+        .next(function(err) {
+          error = err;
           done();
         })
         .dispatch();
     });
     
     it('should error', function() {
-      expect(err).to.be.an.instanceOf(Error);
-      expect(err.constructor.name).to.equal('RouterError');
-      expect(err.message).to.equal('No controller for invalid#index');
+      expect(error).to.be.an.instanceOf(Error);
+      expect(error.message).to.equal("Unable to create controller 'invalid'");
     });
   });
   
