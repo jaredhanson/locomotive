b0e728e8289e29d277c3388d995f5ec74a3d7ebd
Jared Hanson <jaredhanson@gmail.com>
2013-11-18 18:35:39 -0800

    Test case for Controller#param.

--- test/mocks/request.js
+++ test/mocks/request.js
@@ -2,6 +2,11 @@ function MockRequest() {
   this.method = 'GET';
   this.url = '/';
   this.headers = {};
+  this.params = {};
+}
+
+MockRequest.prototype.param = function(name, defaultValue) {
+  return this.params[name] || defaultValue;
 }
 
 
