cc24cdd9814ea6bb517dadd77aff85093f456d76
Jared Hanson <jaredhanson@gmail.com>
2013-11-23 07:33:37 -0800

    More test cases for Controller#error.

--- test/controller.error.test.js
+++ test/controller.error.test.js
@@ -36,4 +36,33 @@ describe('Controller#error', function() {
     });
   });
   
+  describe('implicitly due to exception', function() {
+    var app = new MockApplication();
+    var controller = new Controller();
+    controller.causeTypeError = function() {
+      this.lol.wat;
+    }
+    
+    var req, res, error;
+    
+    before(function(done) {
+      req = new MockRequest();
+      res = new MockResponse(function() {
+        return done(new Error('should not call res#end'));
+      });
+      
+      controller._init(app, 'test');
+      controller._prepare(req, res, function(err) {
+        error = err;
+        return done();
+      });
+      controller._invoke('causeTypeError');
+    });
+    
+    it('should next with error', function() {
+      expect(error).to.be.an.instanceOf(TypeError);
+      expect(error.message).to.match(/wat/);
+    });
+  });
+  
 });
