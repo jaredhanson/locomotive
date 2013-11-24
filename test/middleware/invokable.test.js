1d882f4104e6c82c8d346babbce37a8731c631e6
Jared Hanson <jaredhanson@gmail.com>
2013-11-23 16:48:17 -0800

    More test cases for invokable middleware.

--- test/middleware/invokable.test.js
+++ test/middleware/invokable.test.js
@@ -33,6 +33,42 @@ ReqResController.prototype._invoke = function(action) {
   this.res.end(this.req.url + ' -> ' + this.__id + '#' + action);
 }
 
+function NextController() {
+}
+
+NextController.prototype._init = function(app, id) {
+  this.__app = app;
+  this.__id = id;
+}
+
+NextController.prototype._prepare = function(req, res, next) {
+  this.req = req;
+  this.res = res;
+  this.next = next;
+}
+
+NextController.prototype._invoke = function(action) {
+  this.next();
+}
+
+function ErrorController() {
+}
+
+ErrorController.prototype._init = function(app, id) {
+  this.__app = app;
+  this.__id = id;
+}
+
+ErrorController.prototype._prepare = function(req, res, next) {
+  this.req = req;
+  this.res = res;
+  this.next = next;
+}
+
+ErrorController.prototype._invoke = function(action) {
+  this.next(new Error('something went horribly wrong'));
+}
+
 
 
 describe('middleware/invokable', function() {
@@ -175,6 +211,128 @@ describe('middleware/invokable', function() {
         expect(response.statusCode).to.equal(200);
         expect(response.body).to.equal('/ -> lorem#ipsum');
       });
+    });
+  });
+  
+  
+  describe('invoking controller that calls next', function() {
+    var test, request, response;
+    
+    var app = new MockApplication()
+    app._controllers['lorem'] = NextController;
+
+    before(function(done) {
+      test = chai.connect(invokable(app));
+      test
+        .req(function(req) {
+          request = req;
+        })
+        .res(function(res) {
+          response = res;
+        })
+        .next(function(err) {
+          if (err) { return done(err); }
+          done();
+        })
+        .dispatch();
+    });
+    
+    it('should expose invoke on request', function() {
+      expect(request.invoke).to.be.a('function');
+    });
+    
+    describe('calling invoke', function() {
+      before(function(done) {
+        request.invoke('lorem', 'ipsum', function(err) {
+          if (err) { return done(err); }
+          return done()
+        });
+      });
+      
+      it('should next', function() {
+        // would fail with a timeout if `done()` were not invoked above
+        expect(true).to.equal(true);
+      });
+    });
+  });
+  
+  describe('invoking controller that calls next with error to invoke that does supply callback', function() {
+    var test, request, response;
+    
+    var app = new MockApplication()
+    app._controllers['lorem'] = ErrorController;
+
+    before(function(done) {
+      test = chai.connect(invokable(app));
+      test
+        .req(function(req) {
+          request = req;
+        })
+        .res(function(res) {
+          response = res;
+        })
+        .next(function(err) {
+          if (err) { return done(err); }
+          done();
+        })
+        .dispatch();
+    });
+    
+    it('should expose invoke on request', function() {
+      expect(request.invoke).to.be.a('function');
+    });
+    
+    describe('calling invoke', function() {
+      var error;
+      
+      before(function(done) {
+        request.invoke('lorem', 'ipsum', function(err) {
+          error = err;
+          return done();
+        });
+      });
+      
+      it('should next with error', function() {
+        expect(error).to.be.an.instanceOf(Error);
+        expect(error.message).to.equal('something went horribly wrong');
+      });
+    });
+  });
+  
+  describe('invoking controller that calls next with error to invoke that does not supply callback', function() {
+    var test, request, response;
+    
+    var app = new MockApplication()
+    app._controllers['lorem'] = ErrorController;
+
+    before(function(done) {
+      test = chai.connect(invokable(app));
+      test
+        .req(function(req) {
+          request = req;
+        })
+        .res(function(res) {
+          response = res;
+        })
+        .next(function(err) {
+          if (err) { return done(err); }
+          done();
+        })
+        .dispatch();
+    });
+    
+    it('should expose invoke on request', function() {
+      expect(request.invoke).to.be.a('function');
+    });
+    
+    describe('calling invoke', function() {
+      
+      it('should throw', function() {
+        expect(function() {
+          request.invoke('lorem', 'ipsum');
+        }).to.throw('something went horribly wrong');
+      });
+    
     });
   });
   
