39259bb6a9e7a205a67d9e64816dfe6d25b143ed
Jared Hanson <jaredhanson@gmail.com>
2013-11-23 17:33:47 -0800

    Port helpers test case to mocha.

---
+++ test/helpers/index.test.js
@@ -0,0 +1,17 @@
+var helpers = require('../../lib/locomotive/helpers');
+  
+
+describe('helpers', function() {
+  
+  it('should export tag helpers', function() {
+    expect(helpers.tag).to.be.a('function');
+    expect(helpers.openTag).to.be.a('function');
+    expect(helpers.closeTag).to.be.a('function');
+  });
+  
+  it('should export URL helpers', function() {
+    expect(helpers.linkTo).to.be.a('function');
+    expect(helpers.mailTo).to.be.a('function');
+  });
+  
+});
