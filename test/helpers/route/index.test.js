b242e29a2864ceb41b8518f1613747e6cfc9d196
Jared Hanson <jaredhanson@gmail.com>
2013-11-29 12:19:57 -0800

    Fix to support path substitution with 0 value.

--- test/helpers/route/index.test.js
+++ test/helpers/route/index.test.js
@@ -52,12 +52,14 @@ describe('helpers/route', function() {
     
       it('should build correct path with number', function() {
         expect(pathHelper(7)).to.equal('/songs/7');
+        expect(pathHelper(0)).to.equal('/songs/0');
       });
       it('should build correct path with string', function() {
         expect(pathHelper('mr-jones')).to.equal('/songs/mr-jones');
       });
       it('should build correct path with object id', function() {
         expect(pathHelper({ id: 101 })).to.equal('/songs/101');
+        expect(pathHelper({ id: 0 })).to.equal('/songs/0');
       });
       it('should throw if incorrect number of arguments', function() {
         expect(function() {
@@ -87,12 +89,18 @@ describe('helpers/route', function() {
     
       it('should build correct path with number', function() {
         expect(pathHelper(7, 8)).to.equal('/bands/7/albums/8');
+        expect(pathHelper(0, 8)).to.equal('/bands/0/albums/8');
+        expect(pathHelper(7, 0)).to.equal('/bands/7/albums/0');
+        expect(pathHelper(0, 0)).to.equal('/bands/0/albums/0');
       });
       it('should build correct path with string', function() {
         expect(pathHelper('counting-crows', 'august-and-everything-after')).to.equal('/bands/counting-crows/albums/august-and-everything-after');
       });
       it('should build correct path with object id', function() {
         expect(pathHelper({ id: 101 }, { id: 202 })).to.equal('/bands/101/albums/202');
+        expect(pathHelper({ id: 0 }, { id: 202 })).to.equal('/bands/0/albums/202');
+        expect(pathHelper({ id: 101 }, { id: 0 })).to.equal('/bands/101/albums/0');
+        expect(pathHelper({ id: 0 }, { id: 0 })).to.equal('/bands/0/albums/0');
       });
       it('should throw if incorrect number of arguments', function() {
         expect(function() {
@@ -150,12 +158,14 @@ describe('helpers/route', function() {
     
       it('should build correct URL with number', function() {
         expect(urlHelper(7)).to.equal('http://www.example.com/songs/7');
+        expect(urlHelper(0)).to.equal('http://www.example.com/songs/0');
       });
       it('should build correct URL with string', function() {
         expect(urlHelper('mr-jones')).to.equal('http://www.example.com/songs/mr-jones');
       });
       it('should build correct URL with object id', function() {
         expect(urlHelper({ id: 101 })).to.equal('http://www.example.com/songs/101');
+        expect(urlHelper({ id: 0 })).to.equal('http://www.example.com/songs/0');
       });
       it('should throw if incorrect number of arguments', function() {
         expect(function() {
@@ -185,12 +195,18 @@ describe('helpers/route', function() {
     
       it('should build correct path with number', function() {
         expect(urlHelper(7, 8)).to.equal('http://www.example.com/bands/7/albums/8');
+        expect(urlHelper(0, 8)).to.equal('http://www.example.com/bands/0/albums/8');
+        expect(urlHelper(7, 0)).to.equal('http://www.example.com/bands/7/albums/0');
+        expect(urlHelper(0, 0)).to.equal('http://www.example.com/bands/0/albums/0');
       });
       it('should build correct path with string', function() {
         expect(urlHelper('counting-crows', 'august-and-everything-after')).to.equal('http://www.example.com/bands/counting-crows/albums/august-and-everything-after');
       });
       it('should build correct path with object id', function() {
         expect(urlHelper({ id: 101 }, { id: 202 })).to.equal('http://www.example.com/bands/101/albums/202');
+        expect(urlHelper({ id: 0 }, { id: 202 })).to.equal('http://www.example.com/bands/0/albums/202');
+        expect(urlHelper({ id: 101 }, { id: 0 })).to.equal('http://www.example.com/bands/101/albums/0');
+        expect(urlHelper({ id: 0 }, { id: 0 })).to.equal('http://www.example.com/bands/0/albums/0');
       });
       it('should throw if incorrect number of arguments', function() {
         expect(function() {
