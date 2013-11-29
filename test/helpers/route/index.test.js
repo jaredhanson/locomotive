17956749de77a26e01b269e9210a8231d3c0d2f6
Jared Hanson <jaredhanson@gmail.com>
2013-11-29 11:51:40 -0800

    More test cases for routing helper.

--- test/helpers/route/index.test.js
+++ test/helpers/route/index.test.js
@@ -8,7 +8,7 @@ describe('helpers/route', function() {
   describe('Path', function() {
   
     describe('without placeholder', function() {
-      var urlHelper;
+      var pathHelper;
     
       before(function(done) {
         chai.locomotive.helper(routeHelper('songs', 'index', [], true), 'test', 'show')
@@ -21,13 +21,43 @@ describe('helpers/route', function() {
           })
           .create(function(err, helper) {
             if (err) { return done(err); }
-            urlHelper = helper;
+            pathHelper = helper;
             return done();
           });
       });
     
-      it('should build correct tag to controller action with text', function() {
-        expect(urlHelper()).to.equal('/songs');
+      it('should build correct path', function() {
+        expect(pathHelper()).to.equal('/songs');
+      });
+    });
+    
+    describe('with one placeholder', function() {
+      var pathHelper;
+    
+      before(function(done) {
+        chai.locomotive.helper(routeHelper('songs', 'index', [ 'id' ], true), 'test', 'show')
+          .app(function(app) {
+            app.route('/songs/:id', 'songs', 'index');
+            app.dynamicHelper('urlFor', dynamicHelpers.urlFor);
+          })
+          .req(function(req) {
+            req.headers.host = 'www.example.com';
+          })
+          .create(function(err, helper) {
+            if (err) { return done(err); }
+            pathHelper = helper;
+            return done();
+          });
+      });
+    
+      it('should build correct path with number', function() {
+        expect(pathHelper(7)).to.equal('/songs/7');
+      });
+      it('should build correct path with string', function() {
+        expect(pathHelper('mr-jones')).to.equal('/songs/mr-jones');
+      });
+      it('should build correct path with object id', function() {
+        expect(pathHelper({ id: 101 })).to.equal('/songs/101');
       });
     });
     
@@ -54,8 +84,38 @@ describe('helpers/route', function() {
           });
       });
     
-      it('should build correct tag to controller action with text', function() {
+      it('should build correct URL', function() {
         expect(urlHelper()).to.equal('http://www.example.com/songs');
+      });
+    });
+    
+    describe('with one placeholder', function() {
+      var urlHelper;
+    
+      before(function(done) {
+        chai.locomotive.helper(routeHelper('songs', 'index', [ 'id' ]), 'test', 'show')
+          .app(function(app) {
+            app.route('/songs/:id', 'songs', 'index');
+            app.dynamicHelper('urlFor', dynamicHelpers.urlFor);
+          })
+          .req(function(req) {
+            req.headers.host = 'www.example.com';
+          })
+          .create(function(err, helper) {
+            if (err) { return done(err); }
+            urlHelper = helper;
+            return done();
+          });
+      });
+    
+      it('should build correct path with number', function() {
+        expect(urlHelper(7)).to.equal('http://www.example.com/songs/7');
+      });
+      it('should build correct path with string', function() {
+        expect(urlHelper('mr-jones')).to.equal('http://www.example.com/songs/mr-jones');
+      });
+      it('should build correct path with object id', function() {
+        expect(urlHelper({ id: 101 })).to.equal('http://www.example.com/songs/101');
       });
     });
     
