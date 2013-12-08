eefd6a4a5140458874a18a426748cd672c28a82a
Jared Hanson <jaredhanson@gmail.com>
2013-12-07 18:50:46 -0800

    More test cases for instantiator.

--- test/instantiator.test.js
+++ test/instantiator.test.js
@@ -151,4 +151,103 @@ describe('Instantiator', function() {
     });
   });
   
+  describe('multiple mechanisms', function() {
+    
+    describe('the second of which instantiates, first is sync', function() {
+      var instantiator = new Instantiator();
+      instantiator.use(function(mod) {
+        return;
+      });
+      instantiator.use(function(mod, done) {
+        process.nextTick(function() {
+          var inst = Object.create(mod);
+          inst.attempt = 2;
+          return done(null, inst);
+        });
+      });
+    
+      var instance;
+    
+      before(function(done) {
+        instantiator.instantiate({ bar: 'baz' }, 'foo', function(err, inst) {
+          if (err) { return done(err); }
+          instance = inst;
+          return done();
+        });
+      });
+    
+      it('should instantiate', function() {
+        expect(instance).to.be.an('object');
+        expect(instance.bar).to.equal('baz');
+        expect(instance.attempt).to.equal(2);
+      });
+    });
+    
+    describe('the second of which instantiates, first is async', function() {
+      var instantiator = new Instantiator();
+      instantiator.use(function(mod, done) {
+        process.nextTick(function() {
+          done();
+        });
+      });
+      instantiator.use(function(mod, done) {
+        process.nextTick(function() {
+          var inst = Object.create(mod);
+          inst.attempt = 2;
+          return done(null, inst);
+        });
+      });
+    
+      var instance;
+    
+      before(function(done) {
+        instantiator.instantiate({ bar: 'baz' }, 'foo', function(err, inst) {
+          if (err) { return done(err); }
+          instance = inst;
+          return done();
+        });
+      });
+    
+      it('should instantiate', function() {
+        expect(instance).to.be.an('object');
+        expect(instance.bar).to.equal('baz');
+        expect(instance.attempt).to.equal(2);
+      });
+    });
+    
+    describe('that halt due to error', function() {
+      var instantiator = new Instantiator();
+      instantiator.use(function(mod, done) {
+        process.nextTick(function() {
+          done(new Error('something went wrong'));
+        });
+      });
+      instantiator.use(function(mod, done) {
+        process.nextTick(function() {
+          var inst = Object.create(mod);
+          inst.attempt = 2;
+          return done(null, inst);
+        });
+      });
+    
+      var instance, error;
+    
+      before(function(done) {
+        instantiator.instantiate({ bar: 'baz' }, 'foo', function(err, inst) {
+          error = err;
+          instance = inst;
+          return done();
+        });
+      });
+    
+      it('should error', function() {
+        expect(error).to.be.an.instanceOf(Error);
+        expect(error.message).to.equal('something went wrong');
+      });
+      it('should instantiate', function() {
+        expect(instance).to.be.undefined;
+      });
+    });
+  });
+  
 });
