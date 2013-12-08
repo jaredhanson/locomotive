f68eeb9fd6213b3002bd3b0ab6d9bc5851bc9247
Jared Hanson <jaredhanson@gmail.com>
2013-12-07 17:41:14 -0800

    More test cases for Resolver.

--- test/resolver.test.js
+++ test/resolver.test.js
@@ -14,4 +14,60 @@ describe('Resolver', function() {
     });
   });
   
+  describe('with one mechanism that resolves', function() {
+    var resolver = new Resolver();
+    resolver.use(function(id) {
+      return 'ok-' + id;
+    });
+    
+    it('should resolve using mechanism', function() {
+      expect(resolver.resolve('foo')).to.equal('ok-foo');
+    });
+  });
+  
+  describe('with two mechanisms, the first of which resolves', function() {
+    var resolver = new Resolver();
+    resolver.use(function(id) {
+      return 'ok1-' + id;
+    });
+    resolver.use(function(id) {
+      return 'ok2-' + id;
+    });
+    
+    it('should resolve using mechanism', function() {
+      expect(resolver.resolve('foo')).to.equal('ok1-foo');
+    });
+  });
+  
+  describe('with two mechanisms, the second of which resolves', function() {
+    var resolver = new Resolver();
+    resolver.use(function(id) {
+      return;
+    });
+    resolver.use(function(id) {
+      return 'ok2-' + id;
+    });
+    
+    it('should resolve using mechanism', function() {
+      expect(resolver.resolve('foo')).to.equal('ok2-foo');
+    });
+  });
+  
+  describe('with two mechanisms scoped to prefix', function() {
+    var resolver = new Resolver();
+    resolver.use('foo', function(id) {
+      return 'ok-foo-' + id;
+    });
+    resolver.use('bar', function(id) {
+      return 'ok-bar-' + id;
+    });
+    
+    it('should resolve in foo prefix', function() {
+      expect(resolver.resolve('foo/baz')).to.equal('ok-foo-baz');
+    });
+    it('should resolve in bar prefix', function() {
+      expect(resolver.resolve('bar/baz')).to.equal('ok-bar-baz');
+    });
+  });
+  
 });
