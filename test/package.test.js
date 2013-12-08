618bfe5fcbd7c51d123b509236a86dfcb9d45463
Jared Hanson <jaredhanson@gmail.com>
2013-12-07 18:52:13 -0800

    Test export of boot phases.

--- test/package.test.js
+++ test/package.test.js
@@ -20,4 +20,12 @@ describe('locomotive', function() {
     expect(locomotive.Controller).to.be.a('function');
   });
   
+  it('should export boot phases', function() {
+    expect(locomotive.boot.controllers).to.be.a('function');
+    expect(locomotive.boot.views).to.be.a('function');
+    expect(locomotive.boot.routes).to.be.a('function');
+    expect(locomotive.boot.httpServer).to.be.a('function');
+    expect(locomotive.boot.httpServerCluster).to.be.a('function');
+  });
+  
 });
