c4b22eb5aa4746863dd2c2206c2832b805ecd382
Jared Hanson <jaredhanson@gmail.com>
2013-12-07 17:08:54 -0800

    More test cases for pattern routing helper.

--- test/helpers/route/pattern.test.js
+++ test/helpers/route/pattern.test.js
@@ -59,6 +59,43 @@ describe('helpers/route/pattern', function() {
       });
     });
     
+    describe('with two placeholders', function() {
+      var pathHelper;
+    
+      before(function(done) {
+        chai.locomotive.helper(patternHelper.path('/bands/:band_id/albums/:id'), 'test', 'show')
+          .req(function(req) {
+            req.headers.host = 'www.example.com';
+          })
+          .create(function(err, helper) {
+            if (err) { return done(err); }
+            pathHelper = helper;
+            return done();
+          });
+      });
+    
+      it('should build correct path with number', function() {
+        expect(pathHelper(7, 8)).to.equal('/bands/7/albums/8');
+        expect(pathHelper(0, 8)).to.equal('/bands/0/albums/8');
+        expect(pathHelper(7, 0)).to.equal('/bands/7/albums/0');
+        expect(pathHelper(0, 0)).to.equal('/bands/0/albums/0');
+      });
+      it('should build correct path with string', function() {
+        expect(pathHelper('counting-crows', 'august-and-everything-after')).to.equal('/bands/counting-crows/albums/august-and-everything-after');
+      });
+      it('should build correct path with object id', function() {
+        expect(pathHelper({ id: 101 }, { id: 202 })).to.equal('/bands/101/albums/202');
+        expect(pathHelper({ id: 0 }, { id: 202 })).to.equal('/bands/0/albums/202');
+        expect(pathHelper({ id: 101 }, { id: 0 })).to.equal('/bands/101/albums/0');
+        expect(pathHelper({ id: 0 }, { id: 0 })).to.equal('/bands/0/albums/0');
+      });
+      it('should throw if incorrect number of arguments', function() {
+        expect(function() {
+          pathHelper(7)
+        }).to.throw("Incorrect number of arguments passed to route helper for /bands/:band_id/albums/:id");
+      });
+    });
+    
   });
   
 });
